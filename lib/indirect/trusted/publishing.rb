c47e3a6ccef033b65021ffb7c0909d00c0482a88
Andre Arko <andre@arko.net>
2023-12-08 14:41:00 -0800

    made a gem

---
+++ lib/indirect/trusted/publishing.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require_relative "publishing/version"
+
+module Indirect
+  module Trusted
+    module Publishing
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
---
+++ spec/indirect/trusted/publishing_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Indirect::Trusted::Publishing do
+  it "has a version number" do
+    expect(Indirect::Trusted::Publishing::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
